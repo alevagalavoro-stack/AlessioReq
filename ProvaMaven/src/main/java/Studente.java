f0d454090f7554851268c4c677c00e5bba214210
Alessio <alevagalavoro@gmail.com>
2025-10-22 11:02:27 +0200

    team

---
+++ ProvaMaven/src/main/java/Studente.java
@@ -0,0 +1,25 @@
+
+public class Studente {
+	
+	private String nome;
+	private String matricola;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getMatricola() {
+		return matricola;
+	}
+	public void setMatricola(String matricola) {
+		this.matricola = matricola;
+	}
+	
+		public Studente(String nome, String matricola) {
+			this.nome = nome;
+			this.matricola = matricola;
+		}
+
+}
